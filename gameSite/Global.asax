d0f6bad7ba8c8fb34a6babeaebbf3aeaf89963e9
Hambodini <trimeyers1@gmail.com>
2022-10-10 20:48:38 -0500

    Add project files.

---
+++ gameSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="gameSite.MvcApplication" Language="C#" %>
---
+++ gameSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(gameSite.Startup))]
+namespace gameSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
